89a1b477e96088826882574583f678a06d3eef28
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-24 19:51:55 +0400

    > Initial Commit. > Exploring Linear Layouts.

---
+++ app/src/main/java/com/internshala/helloandroid/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.internshala.helloandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called")
+    }
+}
