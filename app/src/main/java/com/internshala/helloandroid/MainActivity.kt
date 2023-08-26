dfae2a1d700cc7df1d05997c7fbf382123e11057
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-27 01:53:40 +0400

    > Creating Relative Layouts.

--- app/src/main/java/com/internshala/helloandroid/MainActivity.kt
+++ app/src/main/java/com/internshala/helloandroid/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_relative)
         println("onCreate called")
     }
+
 
     override fun onStart() {
         super.onStart()
