922c7e147757d7a3a0ee4f5457dd1ef5f6b38d1b
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-27 17:05:44 +0400

    > Scrolling with Scrollview completed.

--- app/src/main/java/com/internshala/helloandroid/MainActivity.kt
+++ app/src/main/java/com/internshala/helloandroid/MainActivity.kt
@@ -7,7 +7,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_relative)
+        setContentView(R.layout.scrollview_example)
+
         println("onCreate called")
     }
 
---
+++ app/src/main/java/com/internshala/helloandroid/NewActivity.kt
@@ -0,0 +1,18 @@
+package com.internshala.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+        title = "Login"
+
+
+
+    }
+}
