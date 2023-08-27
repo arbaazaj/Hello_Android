c92dc8bfa80d997d625feab6370ed54c20c9ad3f
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-27 17:21:21 +0400

    > Adding Click Listeners completed.

--- app/src/main/java/com/internshala/helloandroid/NewActivity.kt
+++ app/src/main/java/com/internshala/helloandroid/NewActivity.kt
@@ -1,18 +1,37 @@
 package com.internshala.helloandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class NewActivity : AppCompatActivity() {
+class NewActivity : AppCompatActivity(), View.OnClickListener {
 
-
+    lateinit var etMobileNumber: EditText
+    lateinit var etPassword: EditText
+    lateinit var btLogin: Button
+    lateinit var txtForgotPassword: TextView
+    lateinit var txtRegister: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
         title = "Login"
 
+        etMobileNumber = findViewById(R.id.etMobileNumber)
+        etPassword = findViewById(R.id.etPassword)
+        btLogin = findViewById(R.id.btnLogin)
+        txtRegister = findViewById(R.id.txtRegister)
+        txtForgotPassword = findViewById(R.id.txtForgotPassword)
 
+        btLogin.setOnClickListener(this)
 
+    }
+
+    override fun onClick(p0: View?) {
+        Toast.makeText(this@NewActivity, "Button Clicked", Toast.LENGTH_LONG).show()
     }
 }
