c340ce1709d57951bf1db16ecf36499da1cc666b
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-27 20:51:01 +0400

    > Forgot Password screen completed.

--- app/src/main/java/com/internshala/helloandroid/ForgotPassword.kt
+++ app/src/main/java/com/internshala/helloandroid/ForgotPassword.kt
@@ -1,11 +1,33 @@
 package com.internshala.helloandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class ForgotPassword : AppCompatActivity() {
+
+    // Declaring variables.
+    private lateinit var etMobileNumber: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnChangePassword: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        // Finding views by id.
+        etMobileNumber = findViewById(R.id.etMobileNumber)
+        etPassword = findViewById(R.id.etPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        btnChangePassword = findViewById(R.id.btnChangePassword)
+
+        // Button click event.
+        btnChangePassword.setOnClickListener {
+
+        }
+
+
     }
 }
